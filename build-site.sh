9c2ad080a61056858b114fb5017d2b976a411d85
Cameron Roe <croe@archermalmo.com>
2016-07-29 14:56:03 -0500

    Various fixes and added Twitter

--- build-site.sh
+++ build-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 middleman build
-FILE_DIRECTORIES=("build" "build/references" "build/contact" "build/projects")
+FILE_DIRECTORIES=("build" "build/references" "build/contact" "build/projects" "build/thanks")
 for dir in "${FILE_DIRECTORIES[@]}"; do
 	critical "${dir}/index.html" --minify --inline --base "build/" > "${dir}/index-critical.html"
 	mv "${dir}/index-critical.html" "${dir}/index.html"
